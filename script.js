a0549f7a547448b4f792699543456cbe42ea4cf2
yonatan.altaraz <yonatan.altaraz@khealth.ai>
2025-03-22 18:35:32 +0200

    fix

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const files = [
     { file: "assets/mangal_cabin.jpg", description: "A wooden platform built over an old sewer hole to make use of the wasted space." },
     { file: "assets/corner_shelves_thing.jpg", description: "Custom-built wooden corner shelves stand" },
     { file: "assets/cat_stairs.jpg", description: "Stairs for my old cat who has trouble jumping high to reach the window." },
+    { file: "assets/lamp.jpg", description: "A lamp made of a tree branch and 3d printed lampshades, still under construction" },
     { file: "assets/kitchen_counter_and_shelf.jpg", description: "Wooden kitchen counter and shelf" },
     { file: "assets/projector_screen.mp4", description: "A remote-controlled rolling projector screen for my backyard, built from wood and a repurposed remote-controlled bed motor" },
+    { file: "assets/lights_strip.mp4", description: "Custom LED strip with programmable changing color zones, controlled by an Arduino" },
 ];
 
 let currentIndex = 0;
